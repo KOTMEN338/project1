9bbbd69b434d01befc4fef93d75cd1f6640704b7
KOTMEN338 <154454120+KOTMEN338@users.noreply.github.com>
2023-12-20 19:40:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import random
+import os
+
+description = "Эм это я тот самый сын маминой подруги который любит биологию,я был спрограмирован великим человеком.Я разкажу про глобальное потепления"
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+@bot.event
+async def on_ready():
+  print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+print('------')
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def Glnerd(ctx):
+    await ctx.send(f'Эм это я тот самый сын маминой подруги который любит биологию,я был спрограмирован великим человеком.Я разкажу про глобальное потепления:nerd:!')
+@bot.command()
+async def Glcomads(ctx):
+    await ctx.send(f' Мои команды:?Globaltempk ?Globaltempthen ?Globaltempresson и ?Globaltempimg :nerd:.!')
+@bot.command()
+async def Globaltempkr(ctx):
+    await ctx.send(f'Что такое глобальное потепление?Ок сейчас разкажу:Глобальное потепление - это неуклонное повышение средней температуры Земли, измеряемое повышением глобальной температуры поверхности, вызванное или находящееся под влиянием непрерывных выбросов парниковых газов, таких как диоксид углерода и ХФУ, такие как метан и закись азота, а также других загрязнителей воздуха, загрязняющих более 90% атмосферы и влияющих на чувствительность климата и уровень осадков:nerd:!')
+@bot.command()
+async def Globaltempthen(ctx):
+    await ctx.send(f' Основная причина глобального потепления — деятельность человека. Люди сжигают ископаемое топливо (уголь, нефть, газ), в результате чего в атмосферу выбрасываются газы — углекислый газ, метан, оксид азота, фторированные газы. Они ведут к возникновению парникового эффекта, потому что способны поглощать много солнечного тепла. На долю углекислого газа приходится 64% антропогенных причин глобального потепления:nerd:!')
+
+@bot.command()
+async def Globaltempresson(ctx):
+    await ctx.send(f' К последствиям глобального потепления можно отнести:Таяние арктических льдов; Повышение уровня океана; Отступление ледников; Природные катаклизмы (засухи, ураганы, наводнения); Волны тепла и другие квазистационарные погодные состояния; Закисление и деоксигенация океана; Резкое изменение климата:nerd:!')
+@bot.command()
+async def Globaltempimg(ctx):
+  img_name = random.choice(os.listdir('images'))
+  with open(f'images/{img_name}', 'rb') as f:
+    picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+bot.run('MTE4NDUxNDg3Mjg2MDQ5MTg4Ng.G6_yT5.7kvsCrxsDgcNTlocx9NabqVvtwY0EpnfFNO6wI')
+
